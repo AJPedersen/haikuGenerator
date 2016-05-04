80ecccafd45e7f5aba4455dda5e9b7ec368e23e9
aloha <ajp529@gmail.com>
2016-05-03 19:13:18 -0600

    First commit

---
+++ main.js
@@ -0,0 +1,68 @@
+/* A traditional Japanese haiku is a three-line poem with seventeen syllables, written in a 5/7/5 syllable count.
+
+	 Template haikus
+
+	 Follow rules of speech.
+
+	 Trust user to put in correct # of syllables and follow rules of speech/directions. 
+
+	 User input validation using an API? Wordsapi.com
+
+
+HAIKU EXAMPLES:
+
+In the moonlight,
+
+The color and scent of the wisteria
+
+Seems far away.
+
+---
+
+Everything I touch
+
+with tenderness, alas,
+
+pricks like a bramble.
+
+---
+
+The crow has flown away:
+
+swaying in the evening sun,
+
+a leafless tree.  
+
+---
+
+Over-ripe sushi,
+The Master
+Is full of regret.
+
+---
+
+I kill an ant
+and realize my three children
+have been watching.
+
+---
+
+Space is limited
+In a haiku, so it's hard
+To finish what you
+
+--
+
+I see you driving
+Round town with the girl I love
+And I'm like haiku.
+
+---
+
+World is vast and wide.
+So much out there to explore.
+Right now, let's eat lunch.
+
+
+
+ */
